08e7c42605b500fac35000c13d614a5a45c9ad9f
Denis Miagkov <tomich-84@mail.ru>
2023-10-21 00:25:59 +0300

    homework1 added

---
+++ hw1/notes-app/src/com/test1/Main.java
@@ -0,0 +1,10 @@
+package com.test1;
+
+import com.test1.in.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        Console console = new Console();
+        console.start();
+    }
+}
