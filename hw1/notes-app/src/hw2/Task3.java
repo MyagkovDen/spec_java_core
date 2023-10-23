e96f16342436945df85b239ff2f7a7ed9ae5dd7e
Denis Miagkov <tomich-84@mail.ru>
2023-10-23 10:05:20 +0300

    task2 completed

--- hw1/notes-app/src/hw2/Task3.java
+++ hw1/notes-app/src/hw2/Task3.java
@@ -1,5 +1,7 @@
 package hw2;
 
+// Написать функцию, возвращающую истину,
+// если в переданном массиве есть два соседних элемента, с нулевым значением.
 public class Task3 {
     public static void main(String[] args) {
         int [] arr = new int [] {0, 0, 0, 1, 0, 1};
